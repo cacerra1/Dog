4b63f6d28124baa1ccf53a61cbf8bc4e898c70a5
cacerra1 <claudia.acerra@gmail.com>
2018-08-09 08:55:38 -0400

    first few files of Dogs site

---
+++ run.py
@@ -0,0 +1,11 @@
+from app import create_app, db
+from flask_login import LoginManager
+#from app.RiskAssess.old2 import Process
+from sqlalchemy import exc
+from flask_migrate import Migrate, Manager,  MigrateCommand
+
+flask_app = create_app('dev')
+with flask_app.app_context():
+    db.create_all()
+
+    flask_app.run()
